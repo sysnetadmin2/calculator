f1cf8a6d9c58de10792836660b14f7725c5871ff
developer1 <dev1@email.com>
2023-12-15 14:51:55 +0900

    Modify div functuon to check divisor

--- ctrl.py
+++ ctrl.py
@@ -21,3 +21,21 @@ class Control:
 
     def sum(self, a, b):
         return a+b
+
+    def sub(self, a, b):
+        return a-b
+
+    def mul(self, a, b):
+        return a*b
+
+    def div(self, a, b):
+        return a/b
+
+    def pow(self, a, b):
+        return pow(a, b)
+
+    def div(self, a, b):
+        if (b == 0):
+            return 0
+        else:
+            return a/b
